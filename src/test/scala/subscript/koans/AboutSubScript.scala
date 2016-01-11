05f57e860f3e304b203b80236071afe60401ec92
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-01-11 16:54:36 +0200

    One more sample test

--- src/test/scala/subscript/koans/AboutSubScript.scala
+++ src/test/scala/subscript/koans/AboutSubScript.scala
@@ -7,13 +7,22 @@ import subscript.koans.util.KoanSuite
 
 class AboutSubScript extends KoanSuite {
   koan("""Scripts are defined with a `script` keyword.
-       |  Scala code blocks can be used in the script body in the `{! ... !}` braces
-       |  Scripts can be executed using the execute() method.""") {
+         |Scala code blocks can be used in the script body in the `{! ... !}` braces
+         |Scripts can be executed using the execute() method.""") {
     
     var flag = false
     script foo = {!flag = true!}
     runScript(foo)
 
     flag shouldBe true
+  }
+
+  koan("""Scala code blocks can be executed sequentialy using
+         |space as a sequential operator""") {
+    var i = 1
+    script foo = {!i += 1!} {!i += 1!}
+    runScript(foo)
+
+    i shouldBe 3
   }
 }
