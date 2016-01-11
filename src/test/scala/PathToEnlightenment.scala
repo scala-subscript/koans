d2f3a11f81eabe2a4d6b9bcb13202f00988564b1
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-01-11 18:22:58 +0200

    No suites execute after a failure

--- src/test/scala/PathToEnlightenment.scala
+++ src/test/scala/PathToEnlightenment.scala
@@ -5,4 +5,13 @@ import subscript.koans._
 class PathToEnlightenment extends Suites(
   new AboutSubScript
 , new AboutSequentialOperators
-)
+) {
+
+  override def runNestedSuites(args: Args): Status =
+    if (args.distributor.isDefined) runNestedSuites(args.copy(distributor = None))
+    else nestedSuites.foldLeft(SucceededStatus: Status) {(status, suite) =>
+      if  (status == SucceededStatus) suite.run(None, args)
+      else status
+    }
+
+}
