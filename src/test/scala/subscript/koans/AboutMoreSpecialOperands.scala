445f81f6bc856f24470924a16c0187218af7771e
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-01-23 13:07:35 +0200

    More special operands

--- src/test/scala/subscript/koans/AboutMoreSpecialOperands.scala
+++ src/test/scala/subscript/koans/AboutMoreSpecialOperands.scala
@@ -27,4 +27,50 @@ class AboutMoreSpecialOperands extends KoanSuite with OperatorKoansHelper {
     activatedShouldBe(c)
   }
 
+  koan("""`while(p)` is an operand that works like `...`, but loops its
+        | parent sequence until predicate `p` is true.""") {
+    var i = 0
+    script s = while(i < 2) b c {!i += 1!}
+
+    runWithInput(s)(b, c)
+    activatedShouldBe(b)
+
+    runWithInput(s)(b, c, b, c)
+    activatedShouldBe()
+  }
+
+  koan("""As `while` is an operand, it also doesn't matter where you put it
+        | in sequence.""") {
+    var i = 0
+    script s = b while(i < 2) c {!i += 1!}
+
+    runWithInput(s)(b, c)
+    activatedShouldBe(b)
+
+    runWithInput(s)(b, c, b, c)
+    activatedShouldBe()
+  }
+
+  koan("""`break` breaks the activation of its parent operand.""") {
+    script s = b && c && break && d
+
+    runWithInput(s)()
+    activatedShouldBe(b, c)
+  }
+
+  koan("""Optional break `break?` works like `break`, but once an activation
+        | happens in one of its previously activated operands
+        | it resumes its activation sequence.""") {
+    script s = b && c && break? && d
+
+    runWithInput(s)()
+    activatedShouldBe(b, c)
+
+    runWithInput(s)(c)
+    activatedShouldBe(b, d)
+
+    runWithInput(s)(b)
+    activatedShouldBe(c, d)
+  }
+
 }
