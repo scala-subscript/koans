f30f733a55325a79f9d9079c483729ba8cc200b0
AndreVanDelft <andre.vandelft@gmail.com>
2016-02-02 17:50:24 +0100

    Added new Abouts, only as comments FTTB

--- src/test/scala/PathToEnlightenment.scala
+++ src/test/scala/PathToEnlightenment.scala
@@ -7,17 +7,28 @@ class PathToEnlightenment extends Sequential(
 , new AboutScriptDefinitions
 
 , new AboutSequentialOperators
+//, new AboutExclusiveChoice
 , new AboutEpsilonAndDelta
 
 , new AboutAndParallelism
 , new AboutOrParallelism
-, new AboutAlternativeAndDisruption
-, new AboutConditionalOperators
+, new AboutAlternativeAndDisruption // to be split
+//, new AboutDisruption
 
-, new AboutAdvancedSyntax
-, new AboutMoreSpecialOperands
+, new AboutConditionalOperators
+//, new AboutHereAndThere
+//, new AboutIterations // incl break, break?, ..., ..?
+, new AboutAdvancedSyntax // ? still needed
+//, new AboutParentheses // how to get rid of those: call syntax, semicolon, method calls.
+
+//, new AboutAnnotations
+
+//, new AboutCodeFragments // tiny, threaded, unsure, event handling
+//, new AboutTerms // wrap up
 
 , new AboutResultValues
 , new AboutAdvancedResultValues
 , new AboutDataflow
+
+//, new AboutACPAxioms // their relation with SubScript
 )
