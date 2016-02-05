f6a30012eb4040717ef1f894b728dc43d51ee4d7
AndreVanDelft <andre.vandelft@gmail.com>
2016-02-06 00:28:35 +0100

    Improved AboutAndParallelism

--- src/test/scala/subscript/koans/AboutAndParallelism.scala
+++ src/test/scala/subscript/koans/AboutAndParallelism.scala
@@ -8,74 +8,63 @@ import subscript.koans.util._
 class AboutAndParallelism extends KoanSuite with OperatorKoansHelper {
 
   koan(1)(
-    """And-parallel operator `&` executes all its operands in parallel.
-    | It succeeds only if each of its children
-    | succeed - hence and-parallelism.
+    """
+    | The normal parallel operator `&` is also called and-parallel.
+    | It executes all its operands in a parallel way.
+    | The code fragments such as `{! .... !}` will normally not be executed
+    | concurrently, but in an interleaved way: one after another,
+    | a bit like you can shuffle merge a card deck.
     |
-    | `[` and `]` are parentheses specifying operators precedence.
+    | `&` succeeds if and only if all its children have are succeeding.
     |
-    | Space operator has the smallest precedence.
-    | In the following koan, `b c` sequence executes in parallel with
-    | `d e` sequence, this parallelism in sequence with `f`."""
+    | For most operators Scala's precedence rules apply.
+    | Therefore `a b & c + d` is equal to [[a b] & c] d.
+    |
+    | In the following koan, activation of the call to `f` at the end is meant
+    | to signal whether the preceding `&` operator has success
+    | """
   ) {
     script s = [b c & d e] f
 
-    test(1) {
-      runWithInput(s)()
-      activatedShouldBe(__)
-    }
-
-    test(2) {
-      runWithInput(s)(b)
-      activatedShouldBe(__)
-    }
-
-    test(3) {
-      runWithInput(s)(b, c)
-      activatedShouldBe(__)
-    }
-
-    test(4) {
-      runWithInput(s)(b, c, d, e)
-      activatedShouldBe(__)
-    }
-
+    test(1) {runWithInput(s)(       ); activatedShouldBe(__)}
+    test(2) {runWithInput(s)(b      ); activatedShouldBe(__)}
+    test(3) {runWithInput(s)(b,c    ); activatedShouldBe(__)}
+    test(4) {runWithInput(s)(b,c,d,e); activatedShouldBe(__)}
   }
 
   koan(2)(
-    """And-parallel operator `&` doesn't succeed if at least
-    | one of its children doesn't."""
+    """
+    | As soon as one of its children has had a failure, i.e. was deactivated
+    | without having success, then `&` cannot succeed any more.
+    """
   ) {
     script s = [b c & d [-]] f
 
-    test(1) {
-      runWithInput(s)(d)
-      activatedShouldBe(__)
-    }
-
-    test(2) {
-      runWithInput(s)(b, c, d)
-      activatedShouldBe(__)
-    }
+    test(1) {runWithInput(s)(       ); activatedShouldBe(__)}
+    test(2) {runWithInput(s)(      d); activatedShouldBe(__)}
+    test(3) {runWithInput(s)(b,    d); activatedShouldBe(__)}
+    test(4) {runWithInput(s)(b, c, d); activatedShouldBe(__)}
   }
 
   koan(3)(
-    """Non-strict and-parallelism operator `&&` behaves like `&`,
-    | but if one of its children doesn't succeed, it will terminate
-    | without success immediately, terminating all its remaining
-    | operands."""
+    """
+    | `&&` is the so-called 'strong-and-parallel' operator.
+    | This is much like `&`, except that whenever one of the
+    | operands terminates in failure (i.e. without success)
+    | then all other operands are excluded (terminated).
+    |
+    | It is comparable to the boolean operator `&&`, which
+    | is also a stronger version of `&`, in the sense that
+    | it stops evaluation when one operand (the left hand side)
+    | evaluates false.
+    |
+    """
   ) {
     script s = [b c && d [-]] f
 
-    test(1) {
-      runWithInput(s)()
-      activatedShouldBe(__)
-    }
-
-    test(2) {
-      runWithInput(s)(d)
-      activatedShouldBe(__)
-    }
+    test(1) {runWithInput(s)(       ); activatedShouldBe(__)}
+    test(2) {runWithInput(s)(      d); activatedShouldBe(__)}
+    test(3) {runWithInput(s)(b,    d); activatedShouldBe(__)}
+    test(4) {runWithInput(s)(b, c, d); activatedShouldBe(__)}
   }
-
 }
